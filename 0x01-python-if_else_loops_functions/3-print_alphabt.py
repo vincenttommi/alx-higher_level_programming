4b5c9938c24ad8c2660105d15ff74143b06b1b15
vincenttommi <vincenttommi@gmail.com>
2022-09-21 15:17:51 +0300

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Author: vincent tommi korir
 
-for letter in range(87, 123):
+for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
